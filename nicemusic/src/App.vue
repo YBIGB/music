69e39dccceca403317d69cd5ba808cdcf33204fa
YBIGB <1069177921@qq.com>
2020-09-14 22:50:23 +0800

    start

--- nicemusic/src/App.vue
+++ nicemusic/src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-
+  <router-view></router-view>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-   
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
