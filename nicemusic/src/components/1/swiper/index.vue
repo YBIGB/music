f38cd3aad2f9f505373e7e858712493a6cb79ba7
Zambera <1209791748@qq.com>
2020-09-17 09:32:56 +0800

    element 走马灯

---
+++ nicemusic/src/components/1/swiper/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-carousel :interval="4000" type="card" height="300px">
+    <el-carousel-item v-for="item in imagesbox" :key="item.id">
+      <img :src="item.idView" class="image" />
+    </el-carousel-item>
+  </el-carousel>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      imagesbox: [
+        { id: 0, idView: require("../../../assets/images/personal.jpg") },
+        { id: 1, idView: require("../../../assets/images/personal.jpg") },
+        { id: 2, idView: require("../../../assets/images/personal.jpg") },
+        { id: 3, idView: require("../../../assets/images/personal.jpg") },
+      ],
+    };
+  },
+};
+</script>
+<style>
+.image {
+  height: 300px;
+  width: 760px;
+}
+</style>
